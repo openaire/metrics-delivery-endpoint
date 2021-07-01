aea9d3f84e4ee91179880a954f9ef2348d24d172
Marek Horst <mhorst@icm.edu.pl>
2021-07-01 11:29:33 +0200

    Closes #7: Integrate Prometheus RESTful API facade

---
+++ 3rdparty-prometheus4j/src/main/java/com/github/anhdat/PrometheusApiClient.java
@@ -0,0 +1,44 @@
+package com.github.anhdat;
+
+import java.io.IOException;
+
+import com.github.anhdat.models.KeyValResponse;
+import com.github.anhdat.models.MatrixResponse;
+import com.github.anhdat.models.VectorResponse;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class PrometheusApiClient {
+
+    private PrometheusRest service;
+
+    public PrometheusApiClient(String baseUrl) {
+        Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+        service = retrofit.create(PrometheusRest.class);
+    }
+
+    public VectorResponse query(String query) throws IOException {
+        return service.query(query, null, null).execute().body();
+    }
+
+    public VectorResponse query(String query, String time) throws IOException {
+        return service.query(query, time, null).execute().body();
+    }
+
+    public VectorResponse query(String query, String time, String timeout) throws IOException {
+        return service.query(query, time, timeout).execute().body();
+    }
+
+    public MatrixResponse queryRange(String query, String start, String end, String step, String timeout) throws IOException {
+        return service.queryRange(query, start, end, step, timeout).execute().body();
+    }
+
+    public KeyValResponse findSeries(String match, String start, String end) throws IOException {
+        return service.findSeries(match, start, end).execute().body();
+    }
+
+}
