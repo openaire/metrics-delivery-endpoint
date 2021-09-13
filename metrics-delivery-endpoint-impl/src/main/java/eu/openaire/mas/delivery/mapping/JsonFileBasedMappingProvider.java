48714a663214e6a785a4f8f20654e382d72358f3
Marek Horst <mhorst@icm.edu.pl>
2021-09-13 11:01:54 +0200

    Closes #12: spring-boot-devtools should not be used on production
    Removing spring-boot-devtools dependency after reimplementing file syncing to rely directly on the WatchService.

--- metrics-delivery-endpoint-impl/src/main/java/eu/openaire/mas/delivery/mapping/JsonFileBasedMappingProvider.java
+++ metrics-delivery-endpoint-impl/src/main/java/eu/openaire/mas/delivery/mapping/JsonFileBasedMappingProvider.java
@@ -4,21 +4,26 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.devtools.filewatch.ChangedFile;
-import org.springframework.boot.devtools.filewatch.ChangedFile.Type;
-import org.springframework.boot.devtools.filewatch.ChangedFiles;
-import org.springframework.boot.devtools.filewatch.FileChangeListener;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
@@ -32,7 +37,7 @@ import com.google.gson.reflect.TypeToken;
  *
  */
 @Service
-public class JsonFileBasedMappingProvider implements MappingProvider, FileChangeListener {
+public class JsonFileBasedMappingProvider implements MappingProvider {
 
     @Value("${metrics.mapping.location}")
     private Resource fileLocation;
@@ -44,47 +49,31 @@ public class JsonFileBasedMappingProvider implements MappingProvider, FileChange
      */
     private Map<String, Map<String,PrometheusMetricMeta>> metricMappings = new ConcurrentHashMap<String, Map<String,PrometheusMetricMeta>>();
     
+    private ExecutorService fileWatcherExecutorService;
+    
     @PostConstruct
     public void initialize() {
         initializeMappings();
-    }
-
-    private void initializeMappings() {
         try {
-            log.info("loading mappings from file: " + fileLocation.getURI().toString());
-            Map<String, Map<String, PrometheusMetricMeta>> readMap = getMap(getResourceFileAsString(fileLocation));
-            metricMappings.clear();
-            for (Entry<String, Map<String,PrometheusMetricMeta>> entry : readMap.entrySet()) {
-                metricMappings.put(entry.getKey(), new ConcurrentHashMap<String,PrometheusMetricMeta>(entry.getValue()));
-            }
+            WatchService watchService = FileSystems.getDefault().newWatchService();
+            
+            Path path = fileLocation.getFile().getParentFile().toPath();
+
+            path.register(watchService, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_DELETE,
+                    StandardWatchEventKinds.ENTRY_MODIFY);
+            
+            fileWatcherExecutorService = Executors.newSingleThreadExecutor();
+            fileWatcherExecutorService.execute(new MappingFileWatcher(watchService));    
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("unable to initialize mapping watcher service", e);
         }
+        
     }
     
-    @Override
-    public void onChange(Set<ChangedFiles> changeSet) {
-        for(ChangedFiles cFiles : changeSet) {
-            for(ChangedFile cFile: cFiles.getFiles()) {
-                if(isMappingFile(cFile)) {
-                    if (cFile.getType().equals(Type.DELETE)) {
-                        metricMappings.clear();
-                    } else if (cFile.getType().equals(Type.ADD) || cFile.getType().equals(Type.MODIFY)) {
-                        initializeMappings();
-                    }
-                    log.info("Operation: " + cFile.getType() 
-                      + " On file: "+ cFile.getFile().getAbsolutePath() + " is done");
-                    return;
-                }
-            }
-        }
-    }
-    
-    /**
-     * Checks whether notification is related to a mapping file.
-     */
-    private boolean isMappingFile(ChangedFile cFile) {
-        return fileLocation.getFilename().contentEquals(cFile.getRelativeName());
+    @PreDestroy
+    public void destroy() {
+        log.debug("shutting down mapping file watcher");
+        fileWatcherExecutorService.shutdownNow();
     }
     
     @Override
@@ -113,12 +102,41 @@ public class JsonFileBasedMappingProvider implements MappingProvider, FileChange
         }
     }
     
+    /**
+     * Initializes in-memory mappings with the corresponding mapping file contents.
+     */
+    private void initializeMappings() {
+        try {
+            log.info("loading mappings from file: " + fileLocation.getURI().toString());
+            Map<String, Map<String, PrometheusMetricMeta>> readMap = getMap(getResourceFileAsString(fileLocation));
+            metricMappings.clear();
+            for (Entry<String, Map<String,PrometheusMetricMeta>> entry : readMap.entrySet()) {
+                metricMappings.put(entry.getKey(), new ConcurrentHashMap<String,PrometheusMetricMeta>(entry.getValue()));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    /**
+     * Checks whether notification is related to a mapping file.
+     */
+    private boolean isMappingFile(Path mappingFile) {
+        return fileLocation.getFilename().contentEquals(mappingFile.toString());
+    }
+    
+    /**
+     * Converts mappings defined as JSON into the object representation.
+     */
     static Map<String, Map<String, PrometheusMetricMeta>> getMap(String jsonContent) {
         Gson gson = new Gson();
         java.lang.reflect.Type empMapType = new TypeToken<Map<String, Map<String, PrometheusMetricMeta>>>() {}.getType();
         return gson.fromJson(jsonContent, empMapType);
     }
 
+    /**
+     * Loads contents of a given resource into String.
+     */
     static String getResourceFileAsString(Resource resource) throws IOException {
         try (InputStream is = resource.getInputStream()) {
             if (is == null) {
@@ -142,4 +160,53 @@ public class JsonFileBasedMappingProvider implements MappingProvider, FileChange
             return reader.lines().collect(Collectors.joining(System.lineSeparator()));
         }
     }
+    
+    /**
+     * Watcher class responsible for handling mapping file modifications by syncing
+     * in memory mapping state with the file contents.
+     *
+     */
+    class MappingFileWatcher implements Runnable {
+        
+        private WatchService watchService;
+
+        public MappingFileWatcher(WatchService watchService) {
+            this.watchService = watchService;
+        }
+        
+        @Override
+        public void run() {
+            try {
+                WatchKey key;
+                while ((key = watchService.take()) != null) {
+                    for (WatchEvent<?> event : key.pollEvents()) {
+
+                        if (isMappingFile((Path) event.context())) {
+                            if (event.kind().equals(StandardWatchEventKinds.ENTRY_DELETE)) {
+                                log.info("handling file related event by removing all the mappings");
+                                metricMappings.clear();
+                            } else if (event.kind().equals(StandardWatchEventKinds.ENTRY_CREATE)
+                                    || event.kind().equals(StandardWatchEventKinds.ENTRY_MODIFY)) {
+                                log.info("handling file related event by reinitializing the mappings");
+                                initializeMappings();
+                            } else {
+                                log.warn("unsupported event kind: " + event.kind());
+                            }
+                            log.info("hanlding operation: " + event.kind() + " on file: " + event.context() + " is done");
+                        } else {
+                            log.debug("got notification unrelated to the mapping file: " + event.context());
+                        }
+
+                    }
+                    key.reset();
+                }
+            } catch (InterruptedException e) {
+                log.info("got interrupted, finishing mapping file watcher job");
+            }
+
+            log.debug("mapping file watcher job is done");
+        }
+        
+    }
+    
 }
